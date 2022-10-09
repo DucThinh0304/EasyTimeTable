3daa72889c4be8e2adf1423a9419e1ba66ec45a4
DucThinh0304 <90878720+DucThinh0304@users.noreply.github.com>
2022-10-10 04:37:30 +0700

    Add project files.

---
+++ EasyTimeTable/ViewModel/LoginVM/LoginViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EasyTimeTable.Views.LoginWindow;
+
+namespace EasyTimeTable.ViewModel
+{
+    [ObservableObject]
+    public partial class LoginViewModel
+    {
+        public Button LoginBtn { get; set; }
+        public static Frame? MainFrame { get; set; }
+        public Window LoginWindow { get; set; }
+        public ICommand ShadowMaskCM { get; set; }
+        public ICommand CloseWindowCM { get; set; }
+        public ICommand MinimizeWindowCM { get; set; }
+        public ICommand MouseLeftButtonDownWindowCM { get; set; }
+        public ICommand LoadLoginPageCM { get; set; }
+
+        public LoginViewModel()
+        {
+            MouseLeftButtonDownWindowCM = new RelayCommand<Window>((p) =>
+            {
+                if (p != null)
+                {
+                    p.DragMove();
+                }
+            });
+            LoadLoginPageCM = new RelayCommand<Frame>((p) =>
+            {
+                MainFrame = p;
+                p.Content = new LoginPage();
+            });
+        }
+
+            FrameworkElement GetParentWindow(FrameworkElement p)
+        {
+            FrameworkElement? parent = p;
+
+            while (parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+        }
+    }
+}
---
+++ EasyTimeTable/Views/LoginWindow/LoginWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace EasyTimeTable.Views.LoginWindow
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFA5B9D6");
+        }
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = new SolidColorBrush(Colors.Transparent);
+        }
+        private void Button_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFA5B9D6");
+            btn.Background = new SolidColorBrush(Colors.OrangeRed);
+        }
+        private void Button_MouseLeave_1(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Background = new SolidColorBrush(Colors.Transparent);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+    }
+}
